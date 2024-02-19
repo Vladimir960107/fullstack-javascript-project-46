27473ee3941b8f4ed1950b76f86ba77248c239ce
Vladimir <loginov.volodia@gmail.com>
2024-02-19 17:16:58 +0300

    2nd step hexlet

--- gendiff.js
+++ gendiff.js
@@ -6,6 +6,14 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .argument('<filepath1>', 'path to first config file')
+  .argument('<filepath2>', 'path to second config file')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2, options) => {
+    // Здесь будет логика сравнения файлов
+    console.log(`Comparing ${filepath1} and ${filepath2}`);
+    console.log(`Format: ${options.format || 'default'}`);
+  });
 
 program.parse(process.argv);
