a5ae8dc08509ebfdafb22dee6b59d19aace45d18
Vladimir <loginov.volodia@gmail.com>
2024-03-25 12:37:52 +0300

    step 4

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,7 @@ import { Command } from 'commander';
 import fs from 'fs';
 import path from 'path';
 import parseFile from '../src/parsing.js';
+import _ from 'lodash'; 
 
 const program = new Command();
 
@@ -20,24 +21,47 @@ program
   .action((filepath1 , filepath2, options) => {
     // Здесь будет логика сравнения файлов
     console.log(`Comparing ${filepath1} and ${filepath2}`);
+    let obj1 = null;
+    let obj2 = null;
     if (fs.existsSync(filepath1)) {
       console.log('The filepath1 exists.');
-      const file1 = parseFile(filepath1);
-      console.log(file1);
+      obj1 = parseFile(filepath1);
     } else {
       console.log('The filepath1 does not exist.');
     }
     if (fs.existsSync(filepath2)) {
       console.log('The filepath2 exists.');
-      const file2 = parseFile(filepath2);
-      console.log(file2);
+      obj2 = parseFile(filepath2);
     } else {
       console.log('The filepath2 does not exist.');
     }
+
+    if (!_.isNull(obj1) && !_.isNull(obj2)) {
+        const keysUnion = _.union(_.keys(obj1), _.keys(obj2)).sort();
+        let diff = keysUnion.map(key => {
+          if (!_.has(obj2, key)) {
+            return `- ${key}: ${obj1[key]}`;
+          } else if (!_.has(obj1, key)) {
+            return `+ ${key}: ${obj2[key]}`;
+          } else if (obj1[key] !== obj2[key]) {
+            return [`- ${key}: ${obj1[key]}`, `+ ${key}: ${obj2[key]}`].join('\n');
+          } else {
+            return `  ${key}: ${obj1[key]}`;
+          }
+        }).join('\n');
+      
+        console.log(diff);
+    }
+    else {
+        console.log('Cannot compare files.');
+    }
+
     console.log(`Format: ${options.format || 'default'}`);
   });
 
 program.parse(process.argv);
+
+
 
 
 
