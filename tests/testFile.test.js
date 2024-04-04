3082a08e67680f2e3f45596500638295b50c8c06
Vladimir <loginov.volodia@gmail.com>
2024-04-04 11:44:58 +0300

    expected moved to txt files

--- tests/testFile.test.js
+++ tests/testFile.test.js
@@ -1,6 +1,9 @@
+import fs from 'fs';
 import path from 'path';
 import compare from '../src/compare.js';
 import { fileURLToPath } from 'url';
+
+const readFile = (filename) => fs.readFileSync(path.join(dir, '__fixtures__', filename), 'utf-8');
 
 const dir = path.dirname(fileURLToPath(import.meta.url));
 describe('gendiff', () => {
@@ -8,10 +11,7 @@ describe('gendiff', () => {
       const filepath1 = path.join(dir, '__fixtures__', 'data1.json');
       const filepath2 = path.join(dir, '__fixtures__', 'data2.json');
 
-      const expected = `  follow: false
-  host: hexlet.io
-  proxy: 123.234.53.22
-  timeout: 50`;
+      const expected = readFile('expected_no_difference.txt');
       const result = compare(filepath1, filepath2);
       expect(result).toEqual(expected);
     });
@@ -19,12 +19,7 @@ describe('gendiff', () => {
     test('should detect added, removed, and changed keys', () => {
       const filepath1 = path.join(dir, '__fixtures__', 'data1.yaml');
       const filepath2 = path.join(dir, '__fixtures__', 'data2.yaml');
-      const expected = `- follow: false
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: true`;
+      const expected = readFile('expected_difference.txt');
 
       const result = compare(filepath1, filepath2);
 
